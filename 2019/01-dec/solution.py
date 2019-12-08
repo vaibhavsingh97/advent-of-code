c3599ab8e32aacbb32896d08517129e3c7b5bd08
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2019-12-09 00:36:34 +0530

    Day 1 solution :tada:

---
+++ 2019/01-dec/solution.py
@@ -0,0 +1,22 @@
+from math import floor
+
+res = 0
+recursive_res = 0
+
+def get_fuel(mass):
+    return floor(int(mass)/3) - 2
+
+def recursive_get_fuel(mass, sum=0):
+    if mass <= 0:
+        return sum
+    new_fuel = get_fuel(mass)
+    return recursive_get_fuel(new_fuel, sum + max(0, new_fuel))
+
+with open('input.txt') as file:
+    for num in file:
+        mass = int(num)
+        res += get_fuel(mass)
+        recursive_res += recursive_get_fuel(mass)
+
+print(f"Normal fuel sum: {res}")
+print(f"Recursive fuel sum: {recursive_res}")
